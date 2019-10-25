834ebef5f93aa808e11e7c05a8b42dce46bfe120
root <root@LAPTOP-MNRBSD8U.localdomain>
2019-10-25 20:18:00 +0900

    MainInfo Refactorying

--- src/components/PageComponent/MainPages/MainInfo.tsx
+++ src/components/PageComponent/MainPages/MainInfo.tsx
@@ -6,7 +6,7 @@ function MainInfo() {
   return (
     <div className="MainInfo__wrap">
       <div className="MainInfo__Info">
-        <span role="img" aria-label="rocket">🚀</span>
+        <span role="img" aria-label="rocket" className="MainInfo__icon">🚀</span> {/* 임시 */}
         <div className="MainInfo__Catchphrase">소유의 경계를 허물다, 스페이스</div>
         <div className="MainInfo__Subject">누구나, 무엇이든 공유하는 공유경제 마켓</div>
         <div className="MainInfo__Button">
