a152f23fe8163624f6256216924d4515425fc5e8
Doyeong <jeekc0308@gmail.com>
2019-10-26 01:28:54 +0900

    Layout modified.

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./index.css";
+import { Link } from "react-router-dom";
 type LayoutProps = {
   children?: React.ReactNode;
 };
@@ -7,13 +8,18 @@ type LayoutProps = {
 function Layout({ children }: LayoutProps) {
   return (
     <div className="Layout__wrap">
-      <header style={{ padding: '1em' }}>
-        <h1>
-          <span role="img" aria-label="rocket">🚀</span>
-          Space
+      <header className="Layout__header">
+        <h1 style={{ margin: 0 }}>
+          <span role="img" aria-label="rocket">🚀</span>&nbsp;Space
         </h1>
+        <div className="Layout__navContents">
+          <Link to="signin">로그인</Link><span> | </span><Link to="signup">회원가입</Link>
+        </div>
       </header>
       <main className="Layout__contents">{children}</main>
+      <footer className="Layout__footer">
+        Team Surf @ 2019
+      </footer>
     </div>
   );
 }
