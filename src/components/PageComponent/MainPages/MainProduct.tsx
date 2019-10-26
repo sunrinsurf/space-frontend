447ffa25bc2ce7a56955a2391eda429fcffdf163
Doyeong <jeekc0308@gmail.com>
2019-10-27 02:55:56 +0900

    MainShopCard Props

--- src/components/PageComponent/MainPages/MainProduct.tsx
+++ src/components/PageComponent/MainPages/MainProduct.tsx
@@ -8,10 +8,10 @@ function MainProduct() {
             <div className="MainProduct__Info">
                 <div className="MainProduct__Title">최근 공유된 상품</div>
                 <div className="MainProduct__List">
-                    <MainShopCard />
-                    <MainShopCard />
-                    <MainShopCard />
-                    <MainShopCard />
+                    <MainShopCard title="파이어 페스티벌" body="불타오르네" price="월 4억" />
+                    <MainShopCard title="파이어 페스티벌" body="불타오르네" price="월 4억" />
+                    <MainShopCard title="파이어 페스티벌" body="불타오르네" price="월 4억" />
+                    <MainShopCard title="파이어 페스티벌" body="불타오르네" price="월 4억" />
                 </div>
             </div>
         </div >
--- src/components/PageComponent/MainPages/MainShopCard.tsx
+++ src/components/PageComponent/MainPages/MainShopCard.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 import "./MainShopCard.css";
 
-function MainShopCard() {
+function MainShopCard({ title, price, body }: { title: string, price: string, body: string }) {
     return (
         <div className="MainShopCard__wrap">
             <div className="MainShopCard__Image radius"></div>
             <div className="MainShopCard__Content">
-                <div style={{ fontSize: '24px' }}>파이어 페스티벌</div>
-                <div style={{ fontSize: '20px' }}>월 1코인</div>
-                <div>3억~4억 희망</div>
-                {/* props 부탁한다 */}
+                <div style={{ fontSize: '24px' }}>{title}</div>
+                <div style={{ fontSize: '20px' }}>{price}</div>
+                <div>{body}</div>
             </div>
         </div>
     );
