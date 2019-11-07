9aaefc0ba63c1b379468ec09b11c8cc0df7db66a
태민 <48385263+UniTaeMin@users.noreply.github.com>
2019-11-07 22:14:43 +0900

    img 및 title변경
    img 및 title변경

--- src/components/PageComponent/MainPages/MainProduct.tsx
+++ src/components/PageComponent/MainPages/MainProduct.tsx
@@ -9,24 +9,24 @@ function MainProduct() {
         <div className="MainProduct__Title">최근 공유된 상품</div>
         <div className="MainProduct__List">
           <MainShopCard
-            title="파이어 페스티벌"
-            body="불타오르네"
-            price="월 4억"
+            title="에어팟2 공유합니다!"
+            body="별도 문의"
+            price="최대 2인"
           />
           <MainShopCard
-            title="파이어 페스티벌"
-            body="불타오르네"
-            price="월 4억"
+            title="전동 킥보드 같이 타실 분 구해요!"
+            body="날짜/시간대 협의 가능"
+            price="최대 1인"
           />
           <MainShopCard
-            title="파이어 페스티벌"
-            body="불타오르네"
-            price="월 4억"
+            title="넷플릭스 파티원 모집합니다!"
+            body="날짜/시간대 제안 없음"
+            price="최대 3인"
           />
           <MainShopCard
-            title="파이어 페스티벌"
+            title="날짜/시간대 협의 후 결정"
             body="불타오르네"
-            price="월 4억"
+            price="제한 없음"
           />
         </div>
       </div>
--- src/components/PageComponent/MainPages/MainShopCard.tsx
+++ src/components/PageComponent/MainPages/MainShopCard.tsx
@@ -6,7 +6,7 @@ function MainShopCard({ title, price, body }: { title: string, price: string, bo
         <div className="MainShopCard__wrap">
             <div className="MainShopCard__Image radius"></div>
             <div className="MainShopCard__Content">
-                <div style={{ fontSize: '24px' }}>{title}</div>
+                <div style={{ fontSize: '24px', wordBreak: 'keep-all' }}>{title}</div>
                 <div style={{ fontSize: '20px' }}>{price}</div>
                 <div>{body}</div>
             </div>
