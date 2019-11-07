52fefa25c94c01269e91729ab2c0de58c58b6e55
Doyeong <jeekc0308@gmail.com>
2019-11-07 10:58:17 +0900

    production url changed.

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const baseURL = process.env.REACT_APP_LOCAL
   ? "http://127.0.0.1:4000"
-  : "https://iamking.me";
+  : "https://api.surfspace.me";
 const client = axios.create({
   baseURL
 });
