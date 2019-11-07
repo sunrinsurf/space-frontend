2289400a58bb5b558d1fe738328150fa528c02d8
HADMARINE <joonlee031@live.com>
2019-11-07 21:52:46 +0900

    change infopage, added userinfochange

--- src/components/PageComponent/InfoPage/index.tsx
+++ src/components/PageComponent/InfoPage/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ShareList from "../../Shares/ShareList";
+import Button from "../../Form/Button";
 
 interface InfoPageMainProps {
   nickname: string;
@@ -15,12 +16,17 @@ function InfoPageMain({
     <div>
       <h1>안녕하세요, {nickname}님.</h1>
       <section>
+        <h1>내가 참여하고 있는 상품</h1>
+        <ShareList product={invitedProducts} />
+      </section>
+      <section>
         <h1>내가 올린 상품</h1>
         <ShareList product={createdProducts} />
       </section>
       <section>
-        <h1>내가 참여하고 있는 상품</h1>
-        <ShareList product={invitedProducts} />
+        <Button style={{ marginTop: "2em", borderRadius: "25px" }}>
+          회원정보 수정
+        </Button>
       </section>
     </div>
   );
