344974b10a20b55d2ecb5fa6cc54bff2bd92c76f
HADMARINE <joonlee031@live.com>
2019-11-08 08:18:18 +0900

    MainShare 약간 수정

--- src/components/PageComponent/MainPages/MainShareProcedure.tsx
+++ src/components/PageComponent/MainPages/MainShareProcedure.tsx
@@ -1,32 +1,30 @@
 import React from "react";
 import "./MainShareProcedure.css";
-import Arrow from "../../Design/Arrow"
+import Arrow from "../../Design/Arrow";
 
 function MainShareProcedure() {
-    return (
-        <div className="MainShareProcedure__wrap">
-            <div className="MainShareProcedure__Info">
-                <div className="MainShareProcedure__Title">공유 절차 소개</div>
+  return (
+    <div className="MainShareProcedure__wrap">
+      <div className="MainShareProcedure__Info">
+        <div className="MainShareProcedure__Title">공유 절차 소개</div>
 
-                <div className="MainShareProcedure__Image">
-                    <Step start />
-                    <Step />
-                    <Step />
-                </div>
-
-            </div>
+        <div className="MainShareProcedure__Image">
+          <Step start />
+          <Step />
+          <Step />
         </div>
-    );
-
+      </div>
+    </div>
+  );
 }
 
 function Step({ start }: { start?: boolean }) {
-    return (
-        <div className="MainShareProcedure__Images_Element">
-            {!start && <Arrow strokeLen={10} className="MainShareProcedure__arrow"></Arrow>}
-            <div className="MainShareProcedure__Images radius"></div>
-        </div>
-    )
+  return (
+    <div className="MainShareProcedure__Images_Element">
+      {!start && <Arrow strokeLen={10} className="MainShareProcedure__arrow" />}
+      <div className="MainShareProcedure__Images radius"></div>
+    </div>
+  );
 }
 
 export default MainShareProcedure;
