81f190cc0190cf29e9edb75b0ef1089e023a3b40
Doyeong <jeekc0308@gmail.com>
2019-11-07 12:07:42 +0900

    flex layout

--- src/components/FavoriteCategory/index.tsx
+++ src/components/FavoriteCategory/index.tsx
@@ -3,7 +3,7 @@ import FavoriteCategoryCard from './FavoriteCategoryCard'
 
 function FavoriteCategory() {
     return (
-        <div style={{ display: 'flex' }}>
+        <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
             <FavoriteCategoryCard title="인력" />
             <FavoriteCategoryCard title="장소" />
         </div>
