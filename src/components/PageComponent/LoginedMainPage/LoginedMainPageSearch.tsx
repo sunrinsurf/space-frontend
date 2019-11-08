1734f7e54b078976f67bb7aef68d2d9a99432ede
HADMARINE <joonlee031@live.com>
2019-11-08 09:19:30 +0900

    placeholder change

--- src/components/PageComponent/LoginedMainPage/LoginedMainPageSearch.tsx
+++ src/components/PageComponent/LoginedMainPage/LoginedMainPageSearch.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import Input from '../../Form/Input'
-import getClassHandler from '../../../lib/getClassHandler'
-import './LoginedMainPageSearch.css';
+import React from "react";
+import Input from "../../Form/Input";
+import getClassHandler from "../../../lib/getClassHandler";
+import "./LoginedMainPageSearch.css";
 
 const getName = getClassHandler("LoginedMainPageSearch");
 function LoginedMainPageSearch() {
-    return (
-        <div className={getName("wrap")}>
-            <Input placeholder="상품명, 카테고리명 등으로 다양한 재화를 검색해 보세요." />
-        </div>
-    )
+  return (
+    <div className={getName("wrap")}>
+      <Input placeholder="상품명, 카테고리명 등으로 검색해 보세요." />
+    </div>
+  );
 }
 
 export default LoginedMainPageSearch;
