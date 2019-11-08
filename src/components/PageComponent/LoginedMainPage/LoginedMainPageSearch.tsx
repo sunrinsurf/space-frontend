ddf206eb732990ed8ef26f00df03a0a1ee136d3f
HADMARINE <joonlee031@live.com>
2019-11-08 14:36:23 +0900

    Button added

--- src/components/PageComponent/LoginedMainPage/LoginedMainPageSearch.tsx
+++ src/components/PageComponent/LoginedMainPage/LoginedMainPageSearch.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Input from "../../Form/Input";
+import Button from "../../Form/Button";
 import getClassHandler from "../../../lib/getClassHandler";
 import "./LoginedMainPageSearch.css";
 
@@ -7,7 +8,10 @@ const getName = getClassHandler("LoginedMainPageSearch");
 function LoginedMainPageSearch() {
   return (
     <div className={getName("wrap")}>
-      <Input placeholder="상품명, 카테고리명 등으로 검색해 보세요." />
+      <span style={{ display: "flex" }}>
+        <Input placeholder="상품명, 카테고리명 등으로 검색해 보세요." />
+        <Button style={{ height: "47px", marginTop: "12px" }}>검색</Button>
+      </span>
     </div>
   );
 }
