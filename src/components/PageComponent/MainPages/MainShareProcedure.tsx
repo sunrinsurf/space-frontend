a4e790182aefcc093bbc695c923903611b4715ab
태민 <48385263+UniTaeMin@users.noreply.github.com>
2019-12-24 11:23:56 +0900

    공유절차과정
    공유절차과정

--- src/components/PageComponent/MainPages/MainShareProcedure.tsx
+++ src/components/PageComponent/MainPages/MainShareProcedure.tsx
@@ -1,28 +1,52 @@
 import React from "react";
 import "./MainShareProcedure.css";
 import Arrow from "../../Design/Arrow";
+import { url } from "koa-router";
+import styled from "styled-components"
+import step1 from '../../../assets/step/step1.png';
+import step2 from '../../../assets/step/step2.png';
+import step3 from '../../../assets/step/step3.png';
+
+const Steps = styled.img`
+    width: 300px;
+    height: 550px;
+    margin-top: 30px;
+    margin-bottom: 30px;
+    background-repeat: no-repeat;
+    background-size:contain;
+    border-radius: 10px;
+
+`
+const Text = styled.div`
+    text-align:center;
+    font-size:24px;
+    margin-bottom:30px;
+`
 
 function MainShareProcedure() {
   return (
     <div className="MainShareProcedure__wrap">
       <div className="MainShareProcedure__Info">
-        <div className="MainShareProcedure__Title">공유 절차 소개</div>
+        <div className="MainShareProcedure__Title">공유 참여 과정</div>
 
         <div className="MainShareProcedure__Image">
-          <Step start />
-          <Step />
-          <Step />
+          <Step start img={step1} text="공유할 상품 찾기" />
+          <Step img={step2} text="채팅방 참여하기" />
+          <Step img={step3} text="공유 시작" />
         </div>
       </div>
     </div>
   );
 }
 
-function Step({ start }: { start?: boolean }) {
+function Step({ start, img, text }: { start?: boolean, img?: any, text: String }) {
   return (
     <div className="MainShareProcedure__Images_Element">
       {!start && <Arrow strokeLen={10} className="MainShareProcedure__arrow" />}
-      <div className="MainShareProcedure__Images radius"></div>
+      <div>
+        <Steps src={img}></Steps>
+        <Text>{text}</Text>
+      </div>
     </div>
   );
 }
